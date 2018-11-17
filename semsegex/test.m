9bdae0b02b3ba129503db9d1a55510a249cc382b
Sebastian Tauch <sebastian.tauch@uni-ulm.de>
2018-11-17 17:00:21 +0100

    Improved code

--- semsegex/test.m
+++ semsegex/test.m
@@ -1,57 +1,67 @@
-sourceImageFolder = "C:\Users\zorfmorf\Downloads\receptor\RECEPTOR snr 7 density mid"; % absolut path to directory with source images
-sourceGTFile = "C:\Users\zorfmorf\Downloads\receptor\RECEPTOR snr 7 density mid.xml"; % absolut path to directory with source images
+%sourceImageFolder = "C:\Users\zorfmorf\Downloads\receptor\RECEPTOR snr 7 density mid"; % absolut path to directory with source images
+%sourceGTFile = "C:\Users\zorfmorf\Downloads\receptor\RECEPTOR snr 7 density mid.xml"; % absolut path to directory with source images
+sourceGTFile = "/Users/zorfmorf/Projects/uni/neuroproject/semsegex/data/ground_truth/";
+sourceImageFolder = "/Users/zorfmorf/Projects/uni/neuroproject/semsegex/data/RECEPTOR snr 7 density mid";
 
-docGround = xmlread(sourceGTFile);
-image_width = 512;
-image_height = 512;
-generateBlops = 1;
+xmlFiles = dir(strcat(sourceGTFile, '*.xml'));
 
-detection = docGround.getElementsByTagName('detection'); %find all spots
-L = detection.getLength;
-tlength = 100; % starts at one but actually at zero
+for file = xmlFiles'
+    
+    outputname = replace(file.name, ' ', '_');
+    outputname = replace(outputname, '.xml', '');
+    fprintf(1, 'Processing file %s to %s\n', file.name, outputname)
+    docGround = xmlread(strcat(sourceGTFile, file.name));
+    image_width = 512;
+    image_height = 512;
+    generateBlops = 1;
 
-% x, y, t
-% ground truth data image
-gtd = zeros(image_width,image_height,tlength);
+    detection = docGround.getElementsByTagName('detection'); %find all spots
+    L = detection.getLength;
+    tlength = 100; % starts at one but actually at zero
 
-% read spots and write to data
-for k = 1:L
-    fprintf("Handling GT for %d/%d\n", k, L)
-    item = detection.item(k-1); % indices start at 0
-    try
-        x = floor(str2double(item.getAttribute("x"))+1);
-        y = floor(str2double(item.getAttribute("y"))+1);
-        t = floor(str2double(item.getAttribute("t"))+1);
-        
-        % now create circle of ones, faster than doing a loop, couldn't
-        % find a function that does this for me
-        gtd(x,y,t) = 1;
-        gtd(x,y,t) = 1;
-        if generateBlops
-            gtd(x+1,y-1,t) = 1;
-            gtd(x+1,y,t) = 1;
-            gtd(x+1,y+1,t) = 1;
-            gtd(x,y-1,t) = 1;
-            gtd(x,y+1,t) = 1;
-            gtd(x-1,y-1,t) = 1;
-            gtd(x-1,y,t) = 1;
-            gtd(x-1,y+1,t) = 1;
-            gtd(x,y+2,t) = 1;
-            gtd(x,y-2,t) = 1;
-            gtd(x-2,y,t) = 1;
-            gtd(x+2,y,t) = 1;
+    % x, y, t
+    % ground truth data image
+    gtd = zeros(image_width,image_height,tlength);
+
+    % read spots and write to data
+    for k = 1:L
+        %fprintf("Handling GT for %d/%d\n", k, L)
+        item = detection.item(k-1); % indices start at 0
+        try
+            x = floor(str2double(item.getAttribute("x"))+1);
+            y = floor(str2double(item.getAttribute("y"))+1);
+            t = floor(str2double(item.getAttribute("t"))+1);
+
+            % now create circle of ones, faster than doing a loop, couldn't
+            % find a function that does this for me
+            gtd(x,y,t) = 1;
+            if generateBlops
+                try gtd(x+1,y-1,t) = 1; catch end
+                try gtd(x+1,y,t) = 1; catch end
+                try gtd(x+1,y+1,t) = 1; catch end
+                try gtd(x,y-1,t) = 1; catch end
+                try gtd(x,y+1,t) = 1; catch end
+                try gtd(x-1,y-1,t) = 1; catch end
+                try gtd(x-1,y,t) = 1; catch end
+                try gtd(x-1,y+1,t) = 1; catch end
+                try gtd(x,y+2,t) = 1; catch end
+                try gtd(x,y-2,t) = 1; catch end
+                try gtd(x-2,y,t) = 1; catch end
+                try gtd(x+2,y,t) = 1; catch end
+            end
+
+        catch ME
+            fprintf("Could not read values for k = %d\n", k)
+            ME
         end
-        
-    catch ME
-        fprintf("Could not read values for k = %d\n", k)
-        ME
     end
-end
 
-% write to target
-for t=1:tlength
-    name = strcat('groundtruth/', num2str(t), '.png');
-    imwrite(gtd(:,:,t), name)
+    % write to target
+    fprintf(1, 'Writing file targets...\n')
+    for t=1:tlength
+        name = strcat('trainingLabels/', outputname, '_', num2str(t), '.png');
+        imwrite(gtd(:,:,t), name)
+    end
 end
 
 % using a convenient function is wayyyyyyy too slow holy cow botman
