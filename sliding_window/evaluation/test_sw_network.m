a4dd76357bf33971b38921994e09eaac48191b8f
Sebastian Tauch <sebastian.tauch@aborium.com>
2019-03-13 11:05:28 +0100

    Moved some files

--- sliding_window/evaluation/test_sw_network.m
+++ sliding_window/evaluation/test_sw_network.m
@@ -7,8 +7,12 @@ names = [
     
 test_tif = imread("virus_snr7_dens_mid_t0_z05.tif");
 
-GEN_PERCENTAGES = true;
-GEN_IMAGES = false;
+% Test network on respective test set
+GEN_PERCENTAGES = false;
+
+% generate a fit image for one dedicacted sample image
+GEN_IMAGES = true;
+GEN_ORIGINAL = false;
 VERBOSE = false;
 
 for id = 1:numel(names)
@@ -41,7 +45,6 @@ for id = 1:numel(names)
     
     if GEN_PERCENTAGES
         [result, err] = classify(test_net, test_images);
-
 
         test_size = size(result);
 
@@ -95,46 +98,55 @@ for id = 1:numel(names)
         % now slide window through image and add all results together
         siz = size(test_tif, 1); % size of test image
         result = zeros(siz, siz, 3);
-        % TODO remove
-        img = imread("virus_snr7_dens_mid_t0_z05.tif");
-        for i = 1:size(img, 1)
-            for j = 1:size(img, 2)
-                result(i,j,:) = img(i,j);
+        
+        if GEN_ORIGINAL
+            for i = 1:size(test_tif, 1)
+                for j = 1:size(test_tif, 2)
+                    result(i,j,:) = test_tif(i,j);
+                end
             end
         end
         
-        step_size = 3;
-        num_img = (floor((siz - wid)/step_size))^2;
-        sl_stack = zeros(wid, wid, 1, num_img);
-        count = 1;
-        for i=1:step_size:siz-wid
-            for j=1:step_size:siz-wid
-                sl_stack(:, :, 1, count) = test_tif(i:i+wid-1,j:j+wid-1);
-                count = count + 1;
+        if true
+            step_size = 3;
+            num_img = (floor((siz - wid)/step_size))^2;
+            sl_stack = zeros(wid, wid, 1, num_img);
+            count = 1;
+            for i=1:step_size:siz-wid
+                for j=1:step_size:siz-wid
+                    sl_stack(:, :, 1, count) = test_tif(i:i+wid-1,j:j+wid-1);
+                    count = count + 1;
+                end
+            end
+
+            fprintf("\t classifying " + num_img + " images\n");
+            [cl, error] = classify(net, sl_stack);
+
+            fprintf("\t generating result image\n");
+            gauss_fit_stack = zeros(wid, wid,1,1);
+            count = 1;
+            match = 1;
+            for i=1:step_size:siz-wid
+                for j=1:step_size:siz-wid
+                    inc = 0;
+                    if cl(count) == categorical(1)
+                        inc = 1 * error(count);
+                    end
+                    if cl(count) == categorical(2)
+                        inc = 2 * error(count);
+                    end
+                    if inc > 0 && error(count) < 0.2
+                        %result(i+widh,j+widh,2) = 255; %result(i+widh,j+widh,2) + inc;
+                        % add to gauss fit stack
+                        gauss_fit_stack(:,:,1,match) = sl_stack(:, :, 1, count);
+                    end
+                    count = count + 1;
+                end
+                %fprintf("\t\t" + num2str((i/(siz-wid)) * 100) + "%%\n");
             end
         end
-
-        fprintf("\t classifying " + num_img + " images\n");
-        [cl, error] = classify(net, sl_stack);
-
-        fprintf("\t generating result image\n");
-        count = 1;
-        for i=1:step_size:siz-wid
-            for j=1:step_size:siz-wid
-                inc = 0;
-                if cl(count) == categorical(1)
-                    inc = 1 * error(count);
-                end
-                if cl(count) == categorical(2)
-                    inc = 2 * error(count);
-                end
-                if inc > 0 && error(count) < 0.1
-                    result(i+widh,j+widh,2) = 255; %result(i+widh,j+widh,2) + inc;
-                end
-                count = count + 1;
-            end
-            %fprintf("\t\t" + num2str((i/(siz-wid)) * 100) + "%%\n");
-        end
+        
+        
 
         % normalize results so the highest matrix entry is 255
         mmin = min(result(:));
@@ -142,21 +154,23 @@ for id = 1:numel(names)
         result = (result-mmin) ./ (mmax-mmin);
         
         % now write correct points?
-        %c = xml2struct('virus_snr7_dens_mid.xml');
+        c = xml2struct('virus_snr7_dens_mid.xml');
         
-        %length = size(c.root.TrackContestISBI2012.detection, 2);
+        length = size(c.root.TrackContestISBI2012.detection, 2);
         
-        %for i=1:length
-        %    det = c.root.TrackContestISBI2012.detection(i);
-        %    x = str2double(det{1}.Attributes.x);
-        %    y = str2double(det{1}.Attributes.y);
-        %    z = str2double(det{1}.Attributes.z);
-        %    if z > 4 && z < 6
-        %        result(floor(x + 0.5) + 1, floor(y + 0.5) + 1, 1) = 255;
-        %    end
-        %end
+        for i=1:length
+            det = c.root.TrackContestISBI2012.detection(i);
+            x = str2double(det{1}.Attributes.x);
+            y = str2double(det{1}.Attributes.y);
+            z = str2double(det{1}.Attributes.z);
+            if z > 2 && z < 8
+                % NOTE! For  some reason x and y coordinates are switched
+                result(floor(y + 0.5) + 1, floor(x + 0.5) + 1, 1) = 255;
+            end
+        end
         
         % turn into image
         imwrite(result, name + ".tif")
     end
+    return
 end
